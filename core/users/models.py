98621e19c74469289c6d9c91b558b36a8bce0bb4
Fedir <klumenko356@gmail.com>
2025-11-10 18:32:22 +0200

    Model for users (user, admin, moderator)

--- core/users/models.py
+++ core/users/models.py
@@ -1,3 +1,17 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = (
+        ('user', 'User'),
+        ('moderator', 'Moderator'),
+        ('admin', 'Admin'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='user')
+
+    astro_level = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.username
