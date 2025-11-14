973d4f6d8a3288692520772f5d3dca64b8a2e6b0
DurantJr <whod0977@gmail.com>
2025-11-14 21:08:29 +0000

    add local irls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -15,14 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from users.views import home_view, registration_view
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home_view, name='home'),
     path('api/register/', registration_view, name='register'),
     path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('admin/', admin.site.urls),
+    path('api/users/', include('users.urls')),
 ]
